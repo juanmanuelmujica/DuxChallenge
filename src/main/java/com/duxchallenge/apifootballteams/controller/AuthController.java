bd8493b4409396fcb020f7b1401f023246f530fd
jmujica <jmujica@infosis.tech>
2024-09-30 19:32:52 -0300

    chore: add operation description to the api documentation

--- src/main/java/com/duxchallenge/apifootballteams/controller/AuthController.java
+++ src/main/java/com/duxchallenge/apifootballteams/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.duxchallenge.apifootballteams.controller;
 import com.duxchallenge.apifootballteams.data.dto.AuthResponseDto;
 import com.duxchallenge.apifootballteams.data.dto.AuthenticationRequestDto;
 import com.duxchallenge.apifootballteams.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,9 +20,9 @@ public class AuthController {
         this.authService = authService;
     }
 
+    @Operation(summary = "Inicia sesión y devuelve un jwt")
     @PostMapping("login")
     public ResponseEntity<AuthResponseDto> login(@RequestBody AuthenticationRequestDto request) {
         return ResponseEntity.ok(authService.login(request));
     }
-
 }
--- src/main/java/com/duxchallenge/apifootballteams/controller/TeamController.java
+++ src/main/java/com/duxchallenge/apifootballteams/controller/TeamController.java
@@ -2,6 +2,7 @@ package com.duxchallenge.apifootballteams.controller;
 
 import com.duxchallenge.apifootballteams.data.dto.TeamDto;
 import com.duxchallenge.apifootballteams.service.TeamService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,21 +16,25 @@ public class TeamController {
     @Autowired
     private TeamService service;
 
+    @Operation(summary = "Obtiene todos los equipos")
     @GetMapping
     public ResponseEntity<?> getAllTeams() {
         return ResponseEntity.ok(service.getAllTeams());
     }
 
+    @Operation(summary = "Obtiene un equipo por su id")
     @GetMapping("{id}")
     public ResponseEntity<?> getTeamById(@PathVariable(name = "id") int id) {
         return ResponseEntity.ok(service.getTeamById(id));
     }
 
+    @Operation(summary = "Obtiene un equipo por su nombre")
     @GetMapping("buscar")
     public ResponseEntity<?> getTeamById(@RequestParam(name = "nombre") String name) {
         return ResponseEntity.ok(service.getTeamByName(name));
     }
 
+    @Operation(summary = "Guarda un equipo")
     @PostMapping
     public ResponseEntity<?> saveTeam(@RequestBody TeamDto teamDto) throws BadRequestException {
         return ResponseEntity
@@ -37,11 +42,13 @@ public class TeamController {
                 .body(service.saveTeam(teamDto));
     }
 
+    @Operation(summary = "Actualiza un equipo por su id y se envía el objeto con los nuevos datos")
     @PutMapping("{id}")
     public ResponseEntity<?> updateTeam(@PathVariable(name = "id") int id, @RequestBody TeamDto teamDto) {
         return ResponseEntity.ok(service.updateTeam(id, teamDto));
     }
 
+    @Operation(summary = "Elimina un equipo por su id")
     @DeleteMapping("{id}")
     public ResponseEntity<?> deleteTeam(@PathVariable(name = "id") int id) {
         service.deleteTeam(id);
